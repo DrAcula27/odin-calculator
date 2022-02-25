50d911ffe8a3fcb3d0947c448dd31c98b0173efc
Danielle <danielle@kendallspace.net>
2022-02-25 17:01:36 -0500

    styled calculator

---
+++ script.js
@@ -0,0 +1,35 @@
+function add(num1, num2) {
+    return num1 + num2;
+}
+
+function subtract(num1, num2) {
+    return num1 - num2;
+}
+
+function multiply(num1, num2) {
+    return num1 * num2;
+}
+
+function divide(num1, num2) {
+    return num1 / num2;
+}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+        default:
+            alert("please choose either +, -, *, or /");
+    }
+}
+
